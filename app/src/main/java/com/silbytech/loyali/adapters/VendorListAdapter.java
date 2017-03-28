98d40d9fec1266de0033ae54a0d985c5a46f96cf
Yosef Silberhaft <yossisilb@gmail.com>
2017-03-28 15:53:01 +0300

    Vendor Adapter working properly

--- app/src/main/java/com/silbytech/loyali/adapters/VendorListAdapter.java
+++ app/src/main/java/com/silbytech/loyali/adapters/VendorListAdapter.java
@@ -4,10 +4,11 @@ import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 import com.silbytech.loyali.R;
 import com.silbytech.loyali.entities.VendorSerializable;
-
+import com.squareup.picasso.Picasso;
 import java.util.List;
 
 
@@ -19,7 +20,7 @@ public class VendorListAdapter extends BaseAdapter {
     //Deceleration of Variables
     private Context context;
     private List<VendorSerializable> vendorsList;
-    private String phoneNumber;
+    private String MEDIA_URL = "http://192.168.1.14:8000";
 
     public VendorListAdapter(Context context, List<VendorSerializable> vendorsList) {
         this.context = context;
@@ -48,8 +49,11 @@ public class VendorListAdapter extends BaseAdapter {
         TextView storeLocation = (TextView)v.findViewById(R.id.txtViewVendorLocation);
         TextView storePhone = (TextView)v.findViewById(R.id.txtViewPhone);
         TextView storeType = (TextView)v.findViewById(R.id.txtStoreType);
+        ImageView storeLogo = (ImageView)v.findViewById(R.id.imgVendorLogo);
 
-        //Set text for the TextViews
+        String imageURL = MEDIA_URL + vendorsList.get(position).getLogoTitle();
+        System.out.println(imageURL);
+        Picasso.with(context.getApplicationContext()).load(imageURL).into(storeLogo);
         storeName.setText(vendorsList.get(position).getStoreName());
         storeLocation.setText(vendorsList.get(position).getLocation());
         storePhone.setText(vendorsList.get(position).getPhone());
