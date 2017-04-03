5b796f9954b84ca0b03f0b7b65872a57d4949a0f
Yosef Silberhaft <yossisilb@gmail.com>
2017-04-03 22:41:34 +0300

    button change

--- app/src/main/java/com/silbytech/loyali/TempMainMenu.java
+++ app/src/main/java/com/silbytech/loyali/TempMainMenu.java
@@ -2,7 +2,9 @@ package com.silbytech.loyali;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,6 +15,9 @@ public class TempMainMenu extends Activity {
     Button vendors;
     Button btnSplash;
     Button btnViewPager;
+    Button btnInfoTesting;
+    public static final String PREFS = "prefs";
+    private SharedPreferences preferences;
 
 
     @Override
@@ -20,17 +25,14 @@ public class TempMainMenu extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.temp_menu);
 
-        vendors = (Button)findViewById(R.id.TMPbtnVendorList);
+        preferences = getSharedPreferences(PREFS, 0);
+        preferences.edit().putString("customer_id", "8").apply();
+
+
         btnSplash = (Button)findViewById(R.id.TMPSplashbtn);
         btnViewPager = (Button)findViewById(R.id.TMPViewPager);
+        btnInfoTesting = (Button)findViewById(R.id.tmpINFOTesting);
 
-        vendors.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(TempMainMenu.this, VendorListActivity.class);
-                TempMainMenu.this.startActivity(i);
-            }
-        });
 
         btnSplash.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,6 +47,14 @@ public class TempMainMenu extends Activity {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(TempMainMenu.this, MainMenuActivity.class);
+                TempMainMenu.this.startActivity(i);
+            }
+        });
+
+        btnInfoTesting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(TempMainMenu.this, TesterActivity.class);
                 TempMainMenu.this.startActivity(i);
             }
         });
