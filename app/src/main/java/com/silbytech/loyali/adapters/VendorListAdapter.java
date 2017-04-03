117ff17a05d6bb0e6e3920b82d957d9226d242ca
Yosef Silberhaft <yossisilb@gmail.com>
2017-04-03 13:18:49 +0300

    Changed Serializable type and added Card set Text

--- app/src/main/java/com/silbytech/loyali/adapters/VendorListAdapter.java
+++ app/src/main/java/com/silbytech/loyali/adapters/VendorListAdapter.java
@@ -7,7 +7,7 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 import com.silbytech.loyali.R;
-import com.silbytech.loyali.entities.VendorSerializable;
+import com.silbytech.loyali.entities.VendorCardSerializer;
 import com.squareup.picasso.Picasso;
 import java.util.List;
 
@@ -19,10 +19,10 @@ import java.util.List;
 public class VendorListAdapter extends BaseAdapter {
     //Deceleration of Variables
     private Context context;
-    private List<VendorSerializable> vendorsList;
-    private String MEDIA_URL = "http://192.168.1.13:8000";
+    private List<VendorCardSerializer> vendorsList;
+    private String MEDIA_URL = "http://192.168.1.12:8000";
 
-    public VendorListAdapter(Context context, List<VendorSerializable> vendorsList) {
+    public VendorListAdapter(Context context, List<VendorCardSerializer> vendorsList) {
         this.context = context;
         this.vendorsList = vendorsList;
     }
@@ -44,12 +44,14 @@ public class VendorListAdapter extends BaseAdapter {
 
     @Override
     public View getView(final int position, View convertView, ViewGroup parent) {
-        View v = View.inflate(context, R.layout.vendgor_fragment, null);
-        TextView storeName = (TextView)v.findViewById(R.id.txtViewVendorName);
-        TextView storeLocation = (TextView)v.findViewById(R.id.txtViewVendorLocation);
-        TextView storePhone = (TextView)v.findViewById(R.id.txtViewPhone);
-        TextView storeType = (TextView)v.findViewById(R.id.txtStoreType);
-        ImageView storeLogo = (ImageView)v.findViewById(R.id.imgVendorLogo);
+        View v = View.inflate(context, R.layout.vendor_fragment, null);
+        TextView storeName = (TextView) v.findViewById(R.id.txtViewVendorName);
+        TextView storeLocation = (TextView) v.findViewById(R.id.txtViewVendorLocation);
+        TextView storePhone = (TextView) v.findViewById(R.id.txtViewPhone);
+        TextView storeType = (TextView) v.findViewById(R.id.txtStoreType);
+        TextView card1 = (TextView) v.findViewById(R.id.txtCard1);
+        TextView card2 = (TextView) v.findViewById(R.id.txtCard2);
+        ImageView storeLogo = (ImageView) v.findViewById(R.id.imgVendorLogo);
 
         String imageURL = MEDIA_URL + vendorsList.get(position).getLogoTitle();
         System.out.println(imageURL);
@@ -58,6 +60,12 @@ public class VendorListAdapter extends BaseAdapter {
         storeLocation.setText(vendorsList.get(position).getLocation());
         storePhone.setText(vendorsList.get(position).getPhone());
         storeType.setText(vendorsList.get(position).getStoreType());
+        if (vendorsList.get(position).getCards().size() == 2) {
+            card1.setText(vendorsList.get(position).getCards().get(0).getDescription());
+            card2.setText(vendorsList.get(position).getCards().get(1).getDescription());
+        } else if (vendorsList.get(position).getCards().size() == 1){
+            card1.setText(vendorsList.get(position).getCards().get(0).getDescription());
+        }
 
         //Save Vendor id to tag
         v.setTag(vendorsList.get(position).getId());
