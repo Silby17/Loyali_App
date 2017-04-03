28d745c7879998cfaa9c26b53095b0b428aa0812
Yosef Silberhaft <yossisilb@gmail.com>
2017-04-03 22:41:51 +0300

    formatted

--- app/src/main/java/com/silbytech/loyali/LoginActivity.java
+++ app/src/main/java/com/silbytech/loyali/LoginActivity.java
@@ -7,6 +7,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.login.widget.LoginButton;
+
 /************************************
  * Created by Yosef Silberhaft
  ************************************/
@@ -14,18 +18,24 @@ public class LoginActivity extends Activity {
     public static final String PREFS = "prefs";
     Button btnLogin;
     Button btnRegister;
+    LoginButton loginButton;
+    CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.login_layout);
-
-        android.app.FragmentManager fragMan = getFragmentManager();
+        /*android.app.FragmentManager fragMan = getFragmentManager();
         FragmentTransaction ft = fragMan.beginTransaction();
 
         FacebookLoginFragment facebookLoginFragment = new FacebookLoginFragment();
         ft.add(R.id.FB_login_frag, facebookLoginFragment);
         ft.commit();
+//        loginButton = (LoginButton)findViewById(R.id.login)
+        callbackManager = CallbackManager.Factory.create();*/
+
+
 
 
         btnRegister = (Button)findViewById(R.id.btnRegister);
