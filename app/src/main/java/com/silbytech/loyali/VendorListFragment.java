27d4155de6b77243b437c31c0d1b8edc7ca5c5ca
Yosef Silberhaft <yossisilb@gmail.com>
2017-04-03 16:55:23 +0300

    Fully Working

--- app/src/main/java/com/silbytech/loyali/VendorListFragment.java
+++ app/src/main/java/com/silbytech/loyali/VendorListFragment.java
@@ -1,20 +1,27 @@
 package com.silbytech.loyali;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 import com.silbytech.loyali.adapters.VendorListAdapter;
-import com.silbytech.loyali.entities.VendorSerializable;
+import com.silbytech.loyali.entities.VendorCardSerializer;
+import com.silbytech.loyali.responses.MessageResponse;
 import java.util.List;
 import retrofit.Callback;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
-
+import retrofit.mime.TypedByteArray;
 import static com.facebook.FacebookSdk.getApplicationContext;
+
 
 /************************************
  * Created by Yosef Silberhaft
@@ -23,9 +30,13 @@ public class VendorListFragment extends android.support.v4.app.Fragment {
     //Deceleration of Variables
     private ListView lvVendors;
     private VendorListAdapter vendorListAdapter;
-    private List<VendorSerializable> vendorList;
+    private List<VendorCardSerializer> vendorCardsList;
+    public static final String PREFS = "prefs";
+    private SharedPreferences preferences;
+    String customer_id;
 
     public VendorListFragment() {}
+
 
     public static VendorListFragment newInstance(){
         VendorListFragment fragment = new VendorListFragment();
@@ -34,9 +45,12 @@ public class VendorListFragment extends android.support.v4.app.Fragment {
         return fragment;
     }
 
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        preferences = getApplicationContext().getSharedPreferences(PREFS, 0);
+        customer_id = preferences.getString("customer_id", "");
     }
 
 
@@ -44,26 +58,88 @@ public class VendorListFragment extends android.support.v4.app.Fragment {
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-
+        //This Async Task will get all the vendors from the Server and will pass all the info
+        // into the correct adapter
         (new AsyncTask<String, Void, Void>(){
             @Override
-            protected Void doInBackground(String... params) {
+            protected Void doInBackground(String... strings) {
                 final Communicator communicator = new Communicator();
-                communicator.getVendorList(new Callback<List<VendorSerializable>>() {
+                communicator.getVendorsWithCards(new Callback<List<VendorCardSerializer>>() {
+
                     @Override
-                    public void success(List<VendorSerializable> vendorSerializable, Response response) {
-                        System.out.println("Done");
+                    public void success(List<VendorCardSerializer> vendorCardSerializers, Response response) {
                         lvVendors = (ListView)getView().findViewById(R.id.lstViewVendors);
-                        vendorList = vendorSerializable;
-
-                        //Init Adapter
-                        vendorListAdapter = new VendorListAdapter(getApplicationContext(), vendorList);
+                        vendorCardsList = vendorCardSerializers;
+                        vendorListAdapter = new VendorListAdapter(getApplicationContext(), vendorCardsList);
                         lvVendors.setAdapter(vendorListAdapter);
-                    }
 
+                        /****************************************************************************
+                         * On long click of message the user will be prompted to add the current
+                         * vendor to their favourites
+                         ****************************************************************************/
+                        lvVendors.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+                            @Override
+                            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                                final String vendor_id = view.getTag().toString();
+                                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getActivity());
+                                alertDialogBuilder.setTitle(R.string.add_to_favourites);
+
+                                // set dialog message
+                                alertDialogBuilder
+                                        .setMessage(R.string.save_to_favourites)
+                                        .setCancelable(false)
+                                        .setPositiveButton("Yes",new DialogInterface.OnClickListener() {
+                                            public void onClick(DialogInterface dialog,int id) {
+
+                                                //This Async Task will Attempt to make a new subscription
+                                                // between this mobile user and the vendor fragment they
+                                                //have clicked on
+                                                (new AsyncTask<String, Void, Void>(){
+                                                    @Override
+                                                    protected Void doInBackground(String... strings) {
+                                                        Communicator com = new Communicator();
+                                                        com.postCreateSubscription(strings[0], strings[1],
+                                                                new Callback<MessageResponse>() {
+                                                                    @Override
+                                                                    public void success(MessageResponse messageResponse, Response response) {
+                                                                        Toast.makeText(getApplicationContext(),
+                                                                                messageResponse.getMessage(), Toast.LENGTH_SHORT).show();
+                                                                    }
+                                                                    @Override
+                                                                    public void failure(RetrofitError error) {
+                                                                        String json =  new String(((TypedByteArray)error.getResponse().getBody()).getBytes());
+                                                                        String replaced = json.replaceAll("[{}]", "");
+                                                                        String[] msg = replaced.split(":");
+                                                                        Toast.makeText(getApplicationContext(),
+                                                                                msg[1], Toast.LENGTH_SHORT).show();
+                                                                    }
+                                                                });
+                                                        return null;
+                                                    }
+                                                }).execute(vendor_id, customer_id);
+                                            }
+                                        })
+                                        .setNegativeButton("No",new DialogInterface.OnClickListener() {
+                                            public void onClick(DialogInterface dialog,int id) {
+                                                System.out.println("here");
+                                                // if this button is clicked, just close
+                                                // the dialog box and do nothing
+                                                dialog.cancel();
+                                            }
+                                        });
+                                // create alert dialog
+                                AlertDialog alertDialog = alertDialogBuilder.create();
+
+                                // show it
+                                alertDialog.show();
+                                return true;
+                            }
+                        });
+                    }
                     @Override
                     public void failure(RetrofitError error) {
-                        System.out.println("Failed man sorry ");
+                        Toast.makeText(getApplicationContext(),
+                                R.string.connectionError, Toast.LENGTH_SHORT).show();
                     }
                 });
                 return null;
