3d3ebb31aa1b9ecb268fd0a65a8862dbf1bab6b6
Yosef Silberhaft <yossisilb@gmail.com>
2017-04-03 13:22:44 +0300

    Initial Commit

---
+++ app/src/main/java/com/silbytech/loyali/responses/MessageResponse.java
@@ -0,0 +1,25 @@
+package com.silbytech.loyali.responses;
+
+import com.google.gson.annotations.SerializedName;
+import java.io.Serializable;
+
+/************************************
+ * Created by Yosef Silberhaft
+ ************************************/
+public class MessageResponse implements Serializable {
+
+    @SerializedName("message")
+    private String message;
+
+    public MessageResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
