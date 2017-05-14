3baf58b3faf3cda4b7df8bd69a1ed2112ea156fc
Yosef Silberhaft <yossisilb@gmail.com>
2017-05-14 18:20:07 +0300

    Added functionality for Login Button

--- app/src/main/java/com/silbytech/loyali/LoginActivity.java
+++ app/src/main/java/com/silbytech/loyali/LoginActivity.java
@@ -1,44 +1,83 @@
 package com.silbytech.loyali;
 
 import android.app.Activity;
-import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-import com.facebook.CallbackManager;
+import android.widget.EditText;
+import android.widget.Toast;
 import com.facebook.FacebookSdk;
-import com.facebook.login.widget.LoginButton;
+import com.silbytech.loyali.responses.RegisterResponse;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
 /************************************
  * Created by Yosef Silberhaft
  ************************************/
 public class LoginActivity extends Activity {
     public static final String PREFS = "prefs";
+    private SharedPreferences preferences;
     Button btnLogin;
     Button btnRegister;
-    LoginButton loginButton;
-    CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.login_layout);
-        /*android.app.FragmentManager fragMan = getFragmentManager();
-        FragmentTransaction ft = fragMan.beginTransaction();
 
-        FacebookLoginFragment facebookLoginFragment = new FacebookLoginFragment();
-        ft.add(R.id.FB_login_frag, facebookLoginFragment);
-        ft.commit();
-//        loginButton = (LoginButton)findViewById(R.id.login)
-        callbackManager = CallbackManager.Factory.create();*/
-
-
-
-
+        btnLogin = (Button)findViewById(R.id.btnLogin);
         btnRegister = (Button)findViewById(R.id.btnRegister);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String username = ((EditText)findViewById(R.id.edtLoginEmail)).getText().toString();
+                final String password = ((EditText)findViewById(R.id.edtLoginPassword)).getText().toString();
+
+                if(username.equals("") || password.equals("")){
+                    Toast.makeText(getApplicationContext(),
+                            R.string.errorMissingInfo, Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    (new AsyncTask<String, Void, Void>(){
+                        @Override
+                        protected Void doInBackground(String... params) {
+                            Communicator communicator = new Communicator();
+                            communicator.userLogin(params[0], params[1], new Callback<RegisterResponse>() {
+                                @Override
+                                public void success(RegisterResponse resultResponse, Response response) {
+                                    preferences = getSharedPreferences(PREFS, 0);
+                                    preferences.edit().putBoolean("isFacebookUser", false).apply();
+                                    //Adds the email, customer ID and login boolean to Prefs
+                                    preferences.edit().putString("username", username).apply();
+                                    preferences.edit().putString("pass", password).apply();
+                                    preferences.edit().putString("customer_id", Integer.toString(resultResponse.getApiUser().getCustomer_id())).apply();
+                                    preferences.edit().putBoolean("logged-in", true).apply();
+                                    Intent i = new Intent(LoginActivity.this, MainMenuActivity.class);
+                                    LoginActivity.this.startActivity(i);
+                                    finish();
+                                }
+
+                                @Override
+                                public void failure(RetrofitError error) {
+                                    if(error.getResponse().getStatus() == 409){
+                                        Toast.makeText(getApplicationContext(),
+                                                R.string.user_not_exist, Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                            return null;
+                        }
+                    }).execute(username, password);
+                }
+            }
+        });
+
         btnRegister.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
