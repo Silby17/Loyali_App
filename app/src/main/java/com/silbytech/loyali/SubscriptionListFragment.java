eca39ed51f242be4eda9c765316b7bef84f5cb1e
Yosef Silberhaft <yossisilb@gmail.com>
2017-05-15 11:59:22 +0300

    Formatted and fixed Async error handling

--- app/src/main/java/com/silbytech/loyali/SubscriptionListFragment.java
+++ app/src/main/java/com/silbytech/loyali/SubscriptionListFragment.java
@@ -1,4 +1,7 @@
 package com.silbytech.loyali;
+import com.silbytech.loyali.adapters.SubscriptionListAdapter;
+import com.silbytech.loyali.entities.SubscriptionSerializable;
+import static com.facebook.FacebookSdk.getApplicationContext;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
@@ -9,22 +12,19 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
-import com.silbytech.loyali.adapters.SubscriptionListAdapter;
-import com.silbytech.loyali.entities.SubscriptionSerializable;
 import java.util.List;
 import retrofit.Callback;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
-import static com.facebook.FacebookSdk.getApplicationContext;
+
 
 /************************************
  * Created by Yosef Silberhaft
  ************************************/
 public class SubscriptionListFragment extends android.support.v4.app.Fragment {
-    //Deceleration of variables
-    private ListView lvSubscriptions;
-    private SubscriptionListAdapter subscriptionAdapter;
     private List<SubscriptionSerializable> subscriptionList;
+    private SubscriptionListAdapter subscriptionAdapter;
+    private ListView lvSubscriptions;
     public static final String PREFS = "prefs";
     private SharedPreferences preferences;
     String customer_id;
@@ -77,9 +77,19 @@ public class SubscriptionListFragment extends android.support.v4.app.Fragment {
                             }
                             @Override
                             public void failure(RetrofitError error) {
-                                if(error.getResponse().getStatus() == 404){
+                                if(error.getKind().name().equals("NETWORK")){
                                     Toast.makeText(getApplicationContext(),
-                                            R.string.invalidUser, Toast.LENGTH_SHORT).show();
+                                            R.string.networkError, Toast.LENGTH_SHORT).show();
+                                }
+                                else if(error != null){
+                                    if(error.getResponse().getStatus() == 404){
+                                        Toast.makeText(getApplicationContext(),
+                                                R.string.invalidUser, Toast.LENGTH_SHORT).show();
+                                    }
+                                    else{
+                                        Toast.makeText(getApplicationContext(),
+                                                R.string.connectionError, Toast.LENGTH_SHORT).show();
+                                    }
                                 }
                                 else{
                                     Toast.makeText(getApplicationContext(),
