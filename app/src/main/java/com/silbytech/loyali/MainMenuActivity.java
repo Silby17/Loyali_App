f9e95ea052fe6603b90dce519e98f866b9e1de6a
Yosef Silberhaft <yossisilb@gmail.com>
2017-06-01 17:27:58 +0300

    added functionality to the logout menu item

--- app/src/main/java/com/silbytech/loyali/MainMenuActivity.java
+++ app/src/main/java/com/silbytech/loyali/MainMenuActivity.java
@@ -1,5 +1,10 @@
 package com.silbytech.loyali;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
@@ -7,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+
 import com.silbytech.loyali.adapters.ViewPagerAdapter;
 
 /************************************
@@ -14,6 +21,7 @@ import com.silbytech.loyali.adapters.ViewPagerAdapter;
  ************************************/
 public class MainMenuActivity extends AppCompatActivity {
     public static final String PREFS = "prefs";
+    Context context = this;
     private ViewPager viewPager;
     private TabLayout tabLayout;
 
@@ -52,5 +60,48 @@ public class MainMenuActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()){
+            case R.id.itemProfile:
+                Intent i = new Intent(MainMenuActivity.this, UserProfileActivity.class);
+                MainMenuActivity.this.startActivity(i);
+                return true;
+            case R.id.itemLogout:
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+                alertDialogBuilder.setTitle("Logout");
+
+                // set dialog message
+                alertDialogBuilder
+                        .setMessage("Are you sure you want to Logout?")
+                        .setCancelable(false)
+                        .setPositiveButton("Yes",new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                MyApplication.getInstance().clearApplicationData();
+                                SharedPreferences prefs = getSharedPreferences(PREFS, 0);
+                                prefs.edit().clear().apply();
+                                startActivity(new Intent(MainMenuActivity.this, LoginActivity.class));
+                                finish();
+                            }
+                        })
+                        .setNegativeButton("No",new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                // if this button is clicked, just close
+                                // the dialog box and do nothing
+                                dialog.cancel();
+                            }
+                        });
+                // Create Alert Dialog
+                AlertDialog alertDialog = alertDialogBuilder.create();
+
+                // Show the Dialog Box
+                alertDialog.show();
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 }
