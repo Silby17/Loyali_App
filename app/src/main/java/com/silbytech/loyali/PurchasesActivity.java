2fef7edd4eebb8aca6c688b4d028e0608a738f18
Yosef Silberhaft <yossisilb@gmail.com>
2017-06-14 19:14:30 +0300

    Menu options on click working

--- app/src/main/java/com/silbytech/loyali/PurchasesActivity.java
+++ app/src/main/java/com/silbytech/loyali/PurchasesActivity.java
@@ -1,8 +1,16 @@
 package com.silbytech.loyali;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ListView;
 import android.widget.Toast;
 import com.silbytech.loyali.adapters.PurchaseAdapter;
@@ -24,16 +32,24 @@ public class PurchasesActivity extends AppCompatActivity {
     private PurchaseAdapter purchaseAdapter;
     private List<PurchaseSerializer> purchaseList;
     private String customerID;
+    Context context = this;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.purchase_listview);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
         this.lvPurchases = (ListView)findViewById(R.id.lstViewPurchases);
         preferences = getApplicationContext().getSharedPreferences(PREFS, 0);
         //Retrieves the Customer_if from the preferences and stores it locally
         customerID = preferences.getString("customer_id", "");
+
 
         (new AsyncTask<String, Void, Void>(){
             @Override
@@ -56,5 +72,72 @@ public class PurchasesActivity extends AppCompatActivity {
                 return null;
             }
         }).execute(customerID);
+    }
+
+
+    /*************************************************************
+     * This method will override the onBackPressed so that after
+     * the user has punched his card all new updated info
+     * is loaded and seen in the app
+     *************************************************************/
+    @Override
+    public void onBackPressed() {
+        Intent i = new Intent(PurchasesActivity.this, MainMenuActivity.class);
+        this.startActivity(i);
+        this.finish();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            Intent i = new Intent(PurchasesActivity.this, MainMenuActivity.class);
+            PurchasesActivity.this.startActivity(i);
+            finish();
+        }
+        switch(item.getItemId()){
+            case R.id.itemProfile:
+                Intent i = new Intent(PurchasesActivity.this, UserProfileActivity.class);
+                PurchasesActivity.this.startActivity(i);
+                return true;
+            case R.id.itemLogout:
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+                alertDialogBuilder.setTitle("Logout");
+
+                // set dialog message
+                alertDialogBuilder
+                        .setMessage("Are you sure you want to Logout?")
+                        .setCancelable(false)
+                        .setPositiveButton("Yes",new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                MyApplication.getInstance().clearApplicationData();
+                                SharedPreferences prefs = getSharedPreferences(PREFS, 0);
+                                prefs.edit().clear().apply();
+                                startActivity(new Intent(PurchasesActivity.this, LoginActivity.class));
+                                finish();
+                            }
+                        })
+                        .setNegativeButton("No",new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                // if this button is clicked, just close
+                                // the dialog box and do nothing
+                                dialog.cancel();
+                            }
+                        });
+                // Create Alert Dialog
+                AlertDialog alertDialog = alertDialogBuilder.create();
+
+                // Show the Dialog Box
+                alertDialog.show();
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
